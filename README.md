aac53f352e3763cb38a81c1c9b0d8376d6594d8e
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-04-08 12:55:25 -0700

    Initial commit
    Contains programmatic export retrieval and parsing via `export.py`.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Thinklab project exports and analytics
+
+[Thinklab](http://thinklab.com/) is a platform for collaborative online open science. This repository retrieves Thinklab [project exports](http://thinklab.com/discussion/discussion-summary-statistics-for-illustrating-project-impact/191#4). Future releases will perform analytics on the content.
+
+## Usage
+
+The export for a specific project can be retrieved using:
+
+```sh
+python export.py --login login.json --project rephetio --output export/rephetio.json
+```
+
+Users need to create a `login.json` file with their personal Thinklab credentials. ['login-template.json'](login-template.json) provides a template.
+
+## License
+
+Thinklab user content is licensed as [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/ "Creative Commons · Attribution 4.0 International"). Original content in this repository is released under [CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0/ "Creative Commons · CC0 1.0 Universal · Public Domain Dedication").
---
+++ export.py
@@ -0,0 +1,66 @@
+import argparse
+import json
+import datetime
+import os
+
+import requests
+
+def retrieve_project_export(project, username, password):
+    """
+    Retrieve a JSON-formatted Thinklab project export.
+    See http://thinklab.com/d/191#8
+
+    `project` is a Thinklab project id such as `rephetio`.
+    `username` and `password` are Thinklab login credentials.
+    """
+    with requests.Session() as session:
+        login_url = 'http://thinklab.com/login'
+        session.get(login_url)
+        csrf_token = session.cookies['csrftoken']
+        payload = {
+            'email': username,
+            'password': password,
+            'csrfmiddlewaretoken': csrf_token,
+        }
+        session.post(login_url, data=payload)
+
+        export_url = 'http://thinklab.com/p/{}/export.json'.format(project)
+        response = session.get(export_url)
+        export = response.json()
+
+    export['retrieved'] = datetime.datetime.utcnow().isoformat() + 'Z'
+    return export
+
+def save_export(export, path):
+    """
+    Write a JSON export to a text file.
+    """
+    with open(path, 'wt') as write_file:
+        json.dump(export, write_file, ensure_ascii=False, indent=2, sort_keys=True)
+
+def parse_login_json(path):
+    """
+    Parse JSON file with Thinklab login credentials
+    """
+    with open(path) as read_file:
+        login = json.load(read_file)
+    return login['email'], login['password']
+
+if __name__ == '__main__':
+    """
+    Export a Thinklab project to a file.
+
+    Example usage:
+    ```
+    python export.py --login login.json --project rephetio --output export/rephetio.json
+    ```
+    """
+    parser = argparse.ArgumentParser(description='Retrieve Thinklab Project export.')
+    parser.add_argument('--login', default='login.json', help='path to Thinklab login credentials')
+    parser.add_argument('--project', default='rephetio', help='project id')
+    parser.add_argument('--output', default='export.json', help='path to export to')
+    args = parser.parse_args()
+
+    username, password = parse_login_json(args.login)
+    export = retrieve_project_export(args.project, username, password)
+    save_export(export, args.output)
