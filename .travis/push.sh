ee5e6f47bd8e389cf785bcfe68c7609383238b3e
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-08-09 11:28:36 -0400

    Improve Travis CI commit message

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,6 +5,6 @@ git config --global push.default simple
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add export process
-git commit --message "Travis build $TRAVIS_BUILD_NUMBER"
+git commit --message "Retrieve all projects on `date --iso-8601 --universal`\n\nTravis build $TRAVIS_BUILD_NUMBER committed on `date --iso-8601=seconds --universal`."
 git remote add origin https://${GH_TOKEN}@github.com/dhimmel/thinklytics.git > /dev/null 2>&1
 git push
