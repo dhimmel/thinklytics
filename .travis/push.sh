875dabdfd59462b05df16c4330738a99dd6074a0
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-08-09 15:21:33 -0400

    Change Travis email and commit message

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,11 +2,11 @@ set -o errexit
 
 # Inspired by https://gist.github.com/willprice/e07efd73fb7f13f917ea
 git config --global push.default simple
-git config --global user.email "travis@travis-ci.org"
+git config --global user.email "contact@travis-ci.com"
 git config --global user.name "Travis CI"
 git checkout $TRAVIS_BRANCH
 git add export process
-git commit --message "Retrieve all projects on `date --iso-8601 --universal`
+git commit --message "Auto-export all projects on `date --iso-8601 --universal`
 
 Travis build $TRAVIS_BUILD_NUMBER.
 Committed on `date --iso-8601=seconds --universal`."
