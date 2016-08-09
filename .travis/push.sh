66cacd42c673b9c1948ddc3119bb680a703ef302
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-08-09 14:41:01 -0400

    Do not supress stdout/stderr

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,5 +6,5 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add export process
 git commit --message "Retrieve all projects on `date --iso-8601 --universal`\n\nTravis build $TRAVIS_BUILD_NUMBER committed on `date --iso-8601=seconds --universal`."
-git remote add origin https://${GH_TOKEN}@github.com/dhimmel/thinklytics.git > /dev/null 2>&1
+git remote add origin https://${GH_TOKEN}@github.com/dhimmel/thinklytics.git
 git push --set-upstream origin $TRAVIS_BRANCH
