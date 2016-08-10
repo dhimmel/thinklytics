d853f768db97e8bff8f079305e8204a681cde231
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-08-10 10:22:02 -0400

    Do not skip ci in Travis commits
    As described in https://docs.travis-ci.com/user/cron-jobs/:
    
    > Because cron jobs build the latest commit to a particular branch, if that commit
    message includes [ci skip] or [skip ci] the cron job will skip that build.
    
    Therefore, cron jobs were not building on top of previous Travis commits.
    
    Changed Travis CI repo settings to "build pushes" OFF, "build pull requests" OFF
    "limit concurrent jobs" to 1.
    
    Removed `[skip ci]` from .travis.yml.

--- .travis/push.sh
+++ .travis/push.sh
@@ -8,12 +8,8 @@ git checkout $TRAVIS_BRANCH
 git add export process
 git commit --message "Auto-export all projects on `date --iso-8601 --universal`
 
-Created by Travis CI build number $TRAVIS_BUILD_NUMBER and job number $TRAVIS_JOB_NUMBER.
-https://travis-ci.org/dhimmel/thinklytics/builds/$TRAVIS_BUILD_ID
-https://travis-ci.org/dhimmel/thinklytics/jobs/$TRAVIS_JOB_ID
-
+Travis build $TRAVIS_BUILD_NUMBER.
 Committed on `date --iso-8601=seconds --universal`.
-
-[skip ci]"
+"
 git remote set-url origin https://${GH_TOKEN}@github.com/dhimmel/thinklytics.git
-git push --quiet --set-upstream origin $TRAVIS_BRANCH
+git push --set-upstream origin $TRAVIS_BRANCH
