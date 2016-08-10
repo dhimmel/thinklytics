92286d1e735417e9b236024954c0a43ade6398b9
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-08-09 21:39:44 -0400

    Add Travis CI URLs to commit message

--- .travis/push.sh
+++ .travis/push.sh
@@ -8,7 +8,10 @@ git checkout $TRAVIS_BRANCH
 git add export process
 git commit --message "Auto-export all projects on `date --iso-8601 --universal`
 
-Created by Travis CI build number $TRAVIS_BUILD_NUMBER.
+Created by Travis CI build number $TRAVIS_BUILD_NUMBER and job number $TRAVIS_JOB_NUMBER.
+https://travis-ci.org/dhimmel/thinklytics/builds/$TRAVIS_BUILD_ID
+https://travis-ci.org/dhimmel/thinklytics/jobs/$TRAVIS_JOB_ID
+
 Committed on `date --iso-8601=seconds --universal`.
 
 [skip ci]"
